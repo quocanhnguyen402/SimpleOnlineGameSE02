aeed3ce4993f099ab0a33e92fcf36a1c1e9a2e01
H3D-MinhNTC <minhntc@house3d.org>
2018-12-14 00:34:21 +0700

    Minh: demo giao diện

---
+++ frontend/views/layouts/_friend-list.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nguye
+ * Date: 13/12/2018
+ * Time: 11:49 PM
+ */
+?>
+
+<div class="bar-item w3-border-bottom">
+    <div class="friend-avatar">
+        <img class="img" src="https://png.pngtree.com/svg/20161027/service_default_avatar_182956.png">
+    </div>
+    <div class="friend-name">Công Minh</div>
+</div>
+
+<div class="bar-item w3-border-bottom">
+    <div class="friend-avatar">
+        <img class="img" src="https://png.pngtree.com/svg/20161027/service_default_avatar_182956.png">
+    </div>
+    <div class="friend-name">Minh Dũng</div>
+</div>
+
+<div class="bar-item w3-border-bottom">
+    <div class="friend-avatar">
+        <img class="img" src="https://png.pngtree.com/svg/20161027/service_default_avatar_182956.png">
+    </div>
+    <div class="friend-name">Công Anh</div>
+</div>
