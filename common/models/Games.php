a3dab07bcc8aed6dc4a9f65b9986282831fff04e
cd2012hn <huynhminhdung_t61@hus.edu.vn>
2018-12-31 21:14:15 +0700

    dunghm: thêm function để truy vấn điểm

--- common/models/Games.php
+++ common/models/Games.php
@@ -2,12 +2,26 @@
 
 namespace common\models;
 
-
-use yii\helpers\ArrayHelper;
-use Yii;
-
-
 class Games extends base\Games
 {
+    /**
+     * Lấy ra điểm cao nhất của game
+     *
+     * @param int $game_id
+     *
+     * @return  ActiveRecord|boolean   hoặc trả về AR Object, hoặc trả về false
+     */
+    public static function getHighestScore($game_id){
+        $record = Scores::find()
+            ->andWhere([
+                'game_id' => $game_id
+            ])
+            ->orderBy([
+                'scores.score' => SORT_DESC
+            ])
+            ->one();
+
+        return $record?$record:false;
+    }
 
 }
