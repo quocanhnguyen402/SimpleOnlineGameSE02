55a3472e289cab00f4bd4d333e230ff431490e0f
cd2012hn <39496969+cd2012hn@users.noreply.github.com>
2019-01-07 17:52:11 +0700

    dunghm: demo socket.io

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -2,7 +2,17 @@
 
 /* @var $this yii\web\View */
 
+$this->registerJsFile("/socket.io/socket.io.js", [
+        'position' => \yii\web\View::POS_END
+]);
+
+$this->registerJsFile("/js/game.js", [
+    'position' => \yii\web\View::POS_END
+]);
+
 ?>
+
+
 <div class="site-body">
     <div class="container">
 
