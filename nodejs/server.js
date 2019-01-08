1df4ce3416736478ae826c6cdf12661164e644c3
Natashi402 <nguyenquocanh_t61@hus.edu.vn>
2019-01-09 02:50:01 +0700

    Modify node server for games

--- nodejs/server.js
+++ nodejs/server.js
@@ -8,33 +8,29 @@ server.listen(5000, function() {
     console.log('Starting server on port 5000');
 });
 
-//Create some fake record
-var leaderboard = [
-    {
-        player_name: "Natashi",
-        score: 3
-    },
-    {
-        player_name: "Quoc Anh",
-        score: 30
-    }
-];
+//Connect mysql database
+var mysql = require('mysql')
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'se02_game'
+});
+connection.connect()
+
+//Leaderboard from db
+var leaderboard = [];
+getLeaderBoard(connection);
 
 //Array to store rooms
 var rooms_nr_client = [];
-
-//Compare function to use in the sort leaderboad
-function comapre_leaderboard(obj1, obj2) {
-    return obj2.score - obj1.score;
-}
-
 
 //When have a connection
 io.on('connection', function (socket) {
     console.log("New client connected")
     //Call when the client call socket.emit('get_leaderboard', _);
     socket.on('get_leaderboard', function () {
-        console.log(leaderboard);
+        getLeaderBoard(connection);
         //Tell the player to update leaderboard when they submit their score
         socket.emit('update_leaderboard', leaderboard);
     });
@@ -70,6 +66,8 @@ io.on('connection', function (socket) {
             console.log("New connection at room " + room);
             socket.join(room);
             socket.emit('joined', room);
+            socket.emit('another_join');
+            socket.in(room).emit('another_join');
         } else {
             socket.emit('join_failed', room);
         }
@@ -80,8 +78,53 @@ io.on('connection', function (socket) {
         var move = data.move;
         socket.in(room).emit('move', move);
     });
+    //Call when the client call socket.emit('move_up', _);
+    socket.on('move_up', function (room) {
+        //Send to all ppl in room except sender
+        socket.broadcast.to(room).emit('move_down');
+    });
+    //Call when the client call socket.emit('move_down', _);
+    socket.on('move_down', function (room) {
+        //Send to all ppl in room except sender
+        socket.broadcast.to(room).emit('move_up');
+    });
+    //Call when the client call socket.emit('reset_ball', _);
+    socket.on('reset_ball', function (room) {
+        //Create x and y for the ball vector
+        var x = get_random_float(-1, 1);
+        var y = get_random_float(-1, 1);
+        var obj = {
+            x: x,
+            y: y,
+        };
+        //Send to all ppl in room include sender
+        socket.emit('reset_ball', obj);
+        socket.in(room).emit('reset_ball', obj);
+    });
+    socket.on('player_right_score', function (room) {
+        socket.emit('player_right_score', room);
+    });
+    socket.on('player_left_score', function (room) {
+        socket.emit('player_left_score', room);
+    });
 });
 
-// setInterval(function() {
-//     io.sockets.emit('message', 'hi!');
-// }, 1000);
+
+function getLeaderBoard(connection) {
+    connection.query("SELECT user.username as player_name,scores.score as score FROM `scores`,`user` WHERE scores.user_id = user.id AND game_id = 1 ORDER BY scores.score DESC"
+        , function (err, rows, fields) {
+            if (err) throw err;
+            leaderboard = rows;
+    });
+}
+
+// function addToLeaderBoard(connection, data) {
+//     connection.query('INSERT INTO posts SET ?', data, function (error, results, fields) {
+//         // if (error) throw error;
+//         // console.log(results.insertId);
+//     });
+// }
+
+function get_random_float(min, max) {
+    return Math.random() * (max - min) + min;;
+}
