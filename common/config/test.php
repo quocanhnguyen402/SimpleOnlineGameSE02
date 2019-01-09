0f514e965153f72b7a0ccda5cec97c8367a55dd7
H3D-MinhNTC <minhntc@house3d.org>
2019-01-10 01:04:38 +0700

    Minh: fix bug codeception và yii_test không thể chạy cùng lúc

--- common/config/test.php
+++ common/config/test.php
@@ -8,7 +8,7 @@ return [
             'identityClass' => 'common\models\User',
         ],
         'request' => [
-            'cookieValidationKey' => 'test',
+//            'cookieValidationKey' => 'test',
         ],
     ],
 ];
